519be7894bc73270d3aedc77064bde63ded5a6e9
Lee Morgan <lee@leemorgan.dev>
2025-12-22 11:18:38 -0500

    Remove dead code.

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
 import express from "express";
 import compression from "compression";
-import mongoose from "mongoose";
-import cors from "cors";
-import {WebSocketServer} from "ws";
-import http from "http";
 
-import routes from "./routes.js";
-import websockets from "./websockets.js";
 import createHtml from "./createHtml.js";
 
 const app = express();
-const server = http.createServer(app);
-const wss = new WebSocketServer({server, path: "/ws"});
-mongoose.connect("mongodb://127.0.0.1/bingo");
 global.cwd = import.meta.dirname;
-global.html = await createHtml();
+const html = await createHtml();
 
 app.use(compression());
-app.use(express.json());
-app.use(cors());
-
-websockets(wss);
-
-routes(app);
-server.listen(8000);
+app.get("/", (req, res)=>{res.send(html)});
+app.listen(8010);
